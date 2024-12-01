8f94c35986f0c17271623182310c3826b2f8a60d
tutu <t433567423@163.com>
2024-12-01 15:51:06 +0800

    feat: 我的页面大致框架

--- src/mobile/pages/AboutPage.vue
+++ src/mobile/pages/AboutPage.vue
@@ -6,11 +6,43 @@
 -->
 <template>
 	<div class="m-about-wrap">
-		<h2>移动端 关于 组件</h2>
+		<div class="h40vh bg-red"></div>
+		<van-collapse v-model="activeNames">
+			<van-collapse-item title="医管理" :name="1">
+				<van-list>
+					<van-cell v-for="(e, i) in list1" :key="i" :title="e.title" />
+				</van-list>
+			</van-collapse-item>
+			<van-collapse-item title="个人设置" :name="2">
+				<van-list>
+					<van-cell v-for="(e, i) in list2" :key="i" :title="e.title" />
+				</van-list>
+			</van-collapse-item>
+		</van-collapse>
 	</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const activeNames = ref([1, 2]);
+const list1 = [
+	{
+		title: '医院管理'
+	},
+	{
+		title: '医生管理'
+	}
+];
+const list2 = [
+	{
+		title: '个人资料'
+	},
+	{
+		title: '个人检验报告单'
+	}
+];
+</script>
 
 <style lang="less" scoped>
 .m-about-wrap {
